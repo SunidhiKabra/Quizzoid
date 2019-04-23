6859ee0de4c892bb7b86d42717c4c7de56dd756b
Sunidhi Kabra <kabra.sunidhi14.com>
2019-04-23 18:10:50 -0400

    logout functionality added

--- frontEnd/routerForFrontEnd/router.js
+++ frontEnd/routerForFrontEnd/router.js
@@ -1,71 +1,75 @@
 module.exports.routerForFrontEnd = function(app){
 
   app.get('/', function(req, res, next){
-    if(req.session.user === undefined){
-        res.render('index');
+    if(req.session.user === undefined || req.session.user === null){
+        res.render('index', {user: null});
     }
     else{
-      // res.redirect('/home');
+      res.redirect('/home_page');
     }
   });
 
   app.get('/sign_up', function(req, res, next){
     if(req.session.user != undefined){
         req.session.user = null;
+        res.locals.user = null;
     }
-    res.render('sign_up');
+    res.render('sign_up', {user: null});
   });
 
   app.get('/successful_sign_up', function(req, res, next){
-    res.render('successful_sign_up');
+    res.render('successful_sign_up', {user: null});
   });
 
   app.get('/log_in', function(req, res, next){
     if(req.session.user != undefined){
         req.session.user = null;
     }
-    res.render('login');
+    res.status(301).render('login', {user: null});
   });
 
   app.get('/home_page', function(req, res, next){
-    if(req.session.user === undefined){
-        res.redirect('/log_in');
+    if(req.session.user === undefined || req.session.user === null){
+        res.status(301).redirect('/log_in');
       }
     else{
-      res.render('homePage');
+      res.render('homePage', {user: req.session.user});
     }
   });
 
   app.get('/create_quiz', function(req, res, next){
-    if(req.session.user === undefined){
-        res.redirect('/log_in');
+    if(req.session.user === undefined || req.session.user === null){
+        res.status(301).redirect('/log_in');
       }
     else{
-      res.render('createQuiz');
+      res.render('createQuiz', {user: req.session.user});
     }
   });
 
-  // app.get('/start_quiz', function(req, res, next){
-  //   if(req.session.user === undefined){
-  //       res.redirect('/log_in');
-  //     }
-  //   else{
-  //       res.render('startQuiz');
-  //   }
-  // });
-
-  //
   app.get('/start_quiz', function(req, res, next){
-        res.render('startQuiz');
+    if(req.session.user === undefined || req.session.user === null){
+        res.status(301).redirect('/log_in');
+      }
+    else{
+        res.render('startQuiz', {user: req.session.user});
+    }
   });
 
   app.post('/score', function(req, res, next){
-    if(req.session.user === undefined){
-        res.redirect('/log_in');
+    if(req.session.user === undefined || req.session.user === null){
+        res.status(301).redirect('/log_in');
       }
     else{
-      res.render('score', {score: req.body.score, maxScore: req.body.maxScore, percentage: req.body.percentage});
+      res.render('score', {score: req.body.score, maxScore: req.body.maxScore, percentage: req.body.percentage, user: req.session.user});
     }
   });
 
+  app.get('/contact_us', function(req, res, next){
+        res.render('contact', {user: null});
+  });
+
+  app.get('/log_out', function(req, res, next){
+      req.session.user = null;
+      res.status(301).redirect('/');
+  });
 };
